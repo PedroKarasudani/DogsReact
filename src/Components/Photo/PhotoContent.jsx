7ae4997e6a1a532cdefd0ed45828b7ef7999c186
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-07-18 21:13:11 -0300

    estilizando a tela de loading da imagem

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -4,20 +4,25 @@ import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
 import PhotoDelete from './PhotoDelete.jsx';
 import { UserContext } from '../../UserContext.jsx';
+import Image from '../Help/Image.jsx';
 
 const PhotoContent = ({ data }) => {
-  const user = React.useContext(UserContext)
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            {user.data && user.data.username === photo.author ? <PhotoDelete id={photo.id} /> : <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link> }
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.view}>{photo.acessos}</span>
           </p>
           <h1 className="title">
