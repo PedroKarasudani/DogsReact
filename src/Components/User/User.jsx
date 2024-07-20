38bc2db5bce71a0c7c836b969bdd76b1d65aec3f
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-07-20 11:16:32 -0300

    criando pag de erro nas nao encontradas

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -5,17 +5,19 @@ import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStates from './UserStates';
 import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
 
 const User = () => {
-  const {data} = React.useContext(UserContext)
-  
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed user={data.id}/>} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStates />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
